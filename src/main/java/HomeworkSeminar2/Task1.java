e38f5072ceb78e9d338574442a690a3636bd27c5
Vadim Cheremnykh <chere96@yandex.ru>
2023-06-12 20:12:22 +0400

    added task1

---
+++ src/main/java/HomeworkSeminar2/Task1.java
@@ -0,0 +1,37 @@
+package main.java.HomeworkSeminar2;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Task1 {
+    /*
+        Дана строка sql-запроса "select * from students where ".
+        Сформируйте часть WHERE этого запроса, используя StringBuilder.
+        Данные для фильтрации приведены ниже в виде json-строки.
+        Если значение null, то параметр не должен попадать в запрос.
+        Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+         */
+    public static void main(String[] args) {
+        Map <String, String> map = new LinkedHashMap<>();
+        map.put("name", "Ivanov");
+        map.put("country", "Russia");
+        map.put("city", "Moscow");
+        map.put("age", null);
+        System.out.println(map);
+        System.out.println(getQuery(map));
+    }
+
+    public static String getQuery(Map<String, String> params){
+        StringBuilder WHERE = new StringBuilder();
+        for (Map.Entry<String, String> pair: params.entrySet()){
+            if (pair.getValue() != null){
+                WHERE.append(pair.getKey() + " = " + "'" + pair.getValue() + "'" + " and ");
+            }
+        }
+        WHERE.delete(WHERE.length()-4, WHERE.length());
+        return WHERE.toString();
+    }
+}
+
+
+
